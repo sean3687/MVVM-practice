14719e0f050b77c4827f4ee8aa5cdddc59c9b432
sean3687 <yonghyun.jin11@gmail.com>
2021-05-24 14:21:47 +0900

    2021-05-24

--- LiveDataViewModel/app/src/main/java/com/tassiecomp/livedataviewmodelpractice/MainActivity.kt
+++ LiveDataViewModel/app/src/main/java/com/tassiecomp/livedataviewmodelpractice/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener { //View.onclickListener가 implement가 되면
 
     companion object{
         const val TAG:String = "로그"
@@ -41,10 +41,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     //클릭 이벤트가 들어왔을때 발동
     override fun onClick(view: View?) {
         val userInput = userinput_edittext.text.toString().toInt()
+        //onclick은 이제 어느 버튼이 클릭되도 이 fun이 실행된다.
 
         //뷰 모델에 라이브러리 값을 변경한느
-        when(view) {
-            plus_btn ->
+        when(view) { //어떤뷰인지 확인하게 된다.
+            plus_btn -> //plus_btn이면 아래 실행
+                //lateinit var로 생성을 했기때문에 여기서 oncreate에서 만든 view를 엑서스 가능해진것이다.
                 myNumberViewModel.updateValue(actionType = ActionType.PLUS, userInput)// 2가지 input을 넣어준다
             minus_btn ->
                 myNumberViewModel.updateValue(actionType = ActionType.MINUS, userInput)
