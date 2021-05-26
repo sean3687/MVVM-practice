06775767890c04c4cb2804e782e585fe2ea3aec7
sean3687 <yonghyun.jin11@gmail.com>
2021-05-26 21:57:28 +0900

    2021-05-26

--- MVVM/app/src/main/java/com/tassiecomp/mvvmlivedata/MainActivity.kt
+++ MVVM/app/src/main/java/com/tassiecomp/mvvmlivedata/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.tassiecomp.mvvmlivedata
 
+import android.graphics.Paint
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -25,26 +27,61 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        data.add(Todo("숙제"))
-        data.add(Todo("청소",true))
+//        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+//        binding.recyclerView.adapter = TodoAdapter(data,
+//            onClickDeleteIcon = {
+//                deleteTodo(it)
+//            }
+//        )
+        //위에 코드를 아래처럼 간편하게 만들수있다.
 
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding.recyclerView.adapter = TodoAdapter(data)
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = TodoAdapter(data,
+                onClickDeleteIcon = {
+                    deleteTodo(it)
+                },
+                onClickItem = {
+                    toggleTodo(it)
+                }
+            )
+        }
 
+        binding.addButton.setOnClickListener {
+            addTodo()
+        }
 
 
     }
 
-    private fun addTodo(){
+    private fun toggleTodo(todo:Todo){
+        todo.isDone = !todo.isDone
+        binding.recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+    private fun addTodo() {
         val todo = Todo(binding.editText.text.toString())
         data.add(todo)
+        binding.recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+    private fun deleteTodo(todo: Todo) {
+        data.remove(todo)
+        binding.recyclerView.adapter?.notifyDataSetChanged()
     }
 }
 
-data class Todo(val text:String, var isDone:Boolean = false) //기본값 생성
+data class Todo(
+    val text: String,
+    var isDone: Boolean = false
+) //기본값 생성
 
 
-class TodoAdapter(private val myDataset: List<Todo>) :
+class TodoAdapter(
+    private val myDataset: List<Todo>,
+    val onClickDeleteIcon: (todo: Todo) -> Unit,//이 함수자체에 return이 없다는 뜻
+    val onClickItem: (todo: Todo) -> Unit
+) ://이 함수자체에 return이 없다는 뜻
     RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
 
     class TodoViewHolder(val binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root)
@@ -56,7 +93,30 @@ class TodoAdapter(private val myDataset: List<Todo>) :
     }
 
     override fun onBindViewHolder(viewHolder: TodoViewHolder, position: Int) {
-        viewHolder.binding.todoText.text= myDataset[position].text
+        val todo = myDataset[position]
+        //arraylist로 들어오는 값을 하나하나 처리
+        viewHolder.binding.todoText.text = myDataset[position].text
+        //하나하나 들어온값을 체크 하는데 그값이 isDone이면?
+        if (todo.isDone) {
+            viewHolder.binding.todoText.apply {
+                paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG //스타라이크 표시apply
+                setTypeface(null, Typeface.ITALIC)
+            }
+        } else {
+            viewHolder.binding.todoText.apply {
+                paintFlags = 0 // 그냥 normal 표시
+                setTypeface(null, Typeface.NORMAL)
+            }
+
+        }
+
+
+        viewHolder.binding.deleteImage.setOnClickListener {
+            onClickDeleteIcon.invoke(todo)//deleteimage를 눌렀을때
+        }
+        viewHolder.binding.root.setOnClickListener {
+            onClickItem.invoke(todo)// invoke 함수를 실행시킨다는 뜻
+        }
     }
 
     override fun getItemCount() = myDataset.size
