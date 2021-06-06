dda1d6bac5ffa3090ae76797cabc040792ed4fb4
sean3687 <yonghyun.jin11@gmail.com>
2021-06-06 12:10:47 +0900

    2021-06-06

--- leetcode/app/src/main/java/com/tassiecomp/leetpractice/MainActivity.kt
+++ leetcode/app/src/main/java/com/tassiecomp/leetpractice/MainActivity.kt
@@ -9,43 +9,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val main = main(120)
-        Log.d("TAG", "$main")
+        val numbers: IntArray = intArrayOf(1,2,3,4,5,6,7)
+        val result = main(numbers,3)
+        Log.d("TAGGG", "result = $result")
 
     }
 }
 
-fun main(x: Int): Int {
-    val change = "$x"
-    Log.d("calculate", "x = $x")
-
-    val reversed = change.reversed()
-    var str = StringBuilder()
-    Log.d("calculate", "reversed = $reversed")
-    for (i in reversed.indices) { //i in 0..2
-        if (reversed[i] == '-') {
-            str.insert(0, '-') //가장 앞 포지션에 -를 넣는다.
-        } else if (reversed[i] == '0' && i == reversed.length - 1)
-            str.insert(0, "")//0이라면
-
-        else str.append(reversed[i]) //아니라면 뒤에서부터 넣는다.
-    }
-    Log.d("calculate", "str = $str")
-    return str.toString().toInt()
-
-}
-
-class Solutions {
-    fun reverse(x: Int): Int {
-            var y = x.toLong() //long으로 바꿔야 나눌때 0 안나옴 123
-            var result: Long = 0
-            while (y != 0.toLong()) { //input값이 0이 아니면
-                result = result * 10 + y % 10
-                y = y / 10
+fun main(nums: IntArray, k: Int): Unit {
+    class Solution {
+        fun rotate(nums: IntArray, k: Int) {
+            val a = IntArray(nums.size)
+            for (i in nums.indices) {
+                a[(i + k) % nums.size] = nums[i]
             }
-            return if (Int.MAX_VALUE < result || Int.MIN_VALUE > result) 0 else result.toInt()
+            for (i in nums.indices) {
+                nums[i] = a[i]
+            }
+        }
     }
 }
+
+
 
 
 
