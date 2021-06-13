2d5d82447aadb3cbdf186ac869f150198e530119
sean3687 <yonghyun.jin11@gmail.com>
2021-06-13 10:10:00 +0900

    2021-06-12

--- leetcode/app/src/main/java/com/tassiecomp/leetpractice/MainActivity.kt
+++ leetcode/app/src/main/java/com/tassiecomp/leetpractice/MainActivity.kt
@@ -3,7 +3,6 @@ package com.tassiecomp.leetpractice
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +10,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val numbers: IntArray = intArrayOf(1,2,3,4,5,6,7)
-        val result = rotate(numbers,3)
-        Log.d("TAGGG", "result = $result")
+        val array = intArrayOf(6)
+        val response = plusOne(array)
+        Log.d("TAGGG", "result = $response")
 
     }
+
+    fun plusOne(digits: IntArray): IntArray {
+        val answer = arrayListOf<Int>()
+
+        val str = (digits.joinToString().replace(",", "").replace(" ", ""))
+        Log.d("TAGG", "$str")
+        Log.d("TAGG", "length = ${str.length}")
+        Log.d("TAGG", "position1 = ${str.get(1).toString().toInt()}")
+
+
+        val test = (str.toInt() + 1).toString()
+        Log.d("TAGG", "test = $test")
+
+        var i = 0
+        while (i <= test.length-1){
+            val insert = test.get(i).toString().toInt()
+            answer.add(insert)
+            Log.d("TAGG", "$answer")
+            ++i
+        }
+
+        val answerint = answer
+        Log.d("TAGG", "answerint = ${answerint}")
+        return answerint.toIntArray()
+    }
+
+
 }
+
+
+
 
 
 
