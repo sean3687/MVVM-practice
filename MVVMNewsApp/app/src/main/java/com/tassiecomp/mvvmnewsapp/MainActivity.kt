bda237ee033ea7489b58ef15e09923a37029915b
sean3687 <yonghyun.jin11@gmail.com>
2021-06-14 12:13:48 +0900

    2021-06-14

--- MVVMNewsApp/app/src/main/java/com/tassiecomp/mvvmnewsapp/MainActivity.kt
+++ MVVMNewsApp/app/src/main/java/com/tassiecomp/mvvmnewsapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,43 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val job = GlobalScope.launch(Dispatchers.Default) {
-            Log.d(TAG, "Starting long running calculation")
-            withTimeout(3000L) {
-                for (i in 30..40) {
-                    if (isActive) {
-                        Log.d(TAG, "Result for i = $i : ${fib(i)}")
-                    }
-
-                }
-            }
-
-            Log.d(TAG, "Ending Long running calculation")
-
-        }
-
-        runBlocking {
-
-            delay(2000L)
-            job.cancel()
-            Log.d(TAG, "job canceled")
-        }
 
     }
 
-    fun fib(n: Int): Long {
-        return if (n == 0) 0
-        else if (n == 1) 1
-        else fib(n - 1) + fib(n - 2)
-    }
-
-    suspend fun doNetworkCall(): String {
-        delay(3000L)
-        return "this is the answer"
-    }
-
-    suspend fun doNetworkCall2(): String {
-        delay(3000L)
-        return "this is the answer"
-    }
 }
