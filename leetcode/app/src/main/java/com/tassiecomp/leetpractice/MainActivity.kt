e6b1e7b6e127d6f39d3b740cb9a38b954bd532e7
sean3687 <yonghyun.jin11@gmail.com>
2021-06-20 17:39:30 +0900

    2021-06-20

--- leetcode/app/src/main/java/com/tassiecomp/leetpractice/MainActivity.kt
+++ leetcode/app/src/main/java/com/tassiecomp/leetpractice/MainActivity.kt
@@ -12,62 +12,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val s ="hello"
-        val response = firstUniqChar(s)
-        Log.d("TAGGG", "result = $response")
+        val s = "hello"
+//        val response = firstUniqChar(s)
+//        Log.d(TAG, "result = $response")
 
     }
-
-    fun firstUniqChar(s: String): Int {
-        val str = s
-        val array: Array<String> = str.toCharArray().map { it.toString() }.toTypedArray()
-        var i = 0
-        var j = 1
-
-        if (array.size ==1){
-            return 0
-        } else{
-            while(j < array.size){
-              when {
-                array[i]==array[j] -> j++
-
-                i==array.size-2 && j ==array.size-1-> if (array[i]==array[j]){
-                    return -1
-                } else{
-                    return i
-                }
-
-
-
-              }
-
-
-            }
-            return
-        }
-
-    }
-
-
 
 }
-internal class Solution {
-    fun firstUniqChar(s: String): Int {
-        val count = HashMap<Char, Int>()
-        val n = s.length
-        // build hash map : character and how often it appears
-        for (i in 0 until n) {
-            val c = s[i]
-            count[c] = count.getOrDefault(c, 0) + 1
-        }
 
-        // find the index
-        for (i in 0 until n) {
-            if (count[s[i]] == 1) return i
+
+class Solution {
+    fun firstUniqChar(s: String): Int {
+        val hashMap = HashMap<Char, Int>() //비어있는 hashmap을 리턴합니다.
+        val size = s.length
+        for (i in 0 until size) {
+//            val count =
         }
-        return -1
+        return 1
     }
 }
+
 
 
 
