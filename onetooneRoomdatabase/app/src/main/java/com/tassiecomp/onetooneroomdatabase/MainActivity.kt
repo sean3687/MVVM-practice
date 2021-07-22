e467016951903e1ee5c0a3987a00b42f529e8632
sean3687 <yonghyun.jin11@gmail.com>
2021-07-22 13:20:36 +0900

    2021-07-22

--- onetooneRoomdatabase/app/src/main/java/com/tassiecomp/onetooneroomdatabase/MainActivity.kt
+++ onetooneRoomdatabase/app/src/main/java/com/tassiecomp/onetooneroomdatabase/MainActivity.kt
@@ -1,10 +1,9 @@
-package com.tassiecomp.roomdatabasepractice
+package com.tassiecomp.onetooneroomdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
