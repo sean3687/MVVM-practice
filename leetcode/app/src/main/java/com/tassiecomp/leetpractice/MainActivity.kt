98ae2cdb32c01d784622f765a74650e551dd64f4
sean3687 <yonghyun.jin11@gmail.com>
2021-07-28 16:45:03 +0900

    2021-07-28

--- leetcode/app/src/main/java/com/tassiecomp/leetpractice/MainActivity.kt
+++ leetcode/app/src/main/java/com/tassiecomp/leetpractice/MainActivity.kt
@@ -94,4 +94,50 @@ internal class LinkedList {
         +----+------+     +----+------+     +----+------+ */
         }
     }
+
+
+    fun insertAfter(prev_node: Node?, new_data: Int) {
+        /* 1. Node가 null인지 확인합니다.  */
+       //prev_node는 C ABCD
+        //new_date는 E ABECD
+        if (prev_node == null) {
+            println("The given previous node cannot be null")
+            return
+        }
+
+        /* 2. val new_node를 new_data 로 지정합니다.
+    3. 새로 넣어줄 new_data가 노드가 됩니다.*/
+        val new_node = Node(new_data) //E
+
+        /* 4. new_node 다음에 올 node는 현C의 다음자리 D  */
+        new_node.next = prev_node.next //
+
+        /* 5. prev_node의 다음자리는 new_node */
+        prev_node.next = new_node
+    }
+
+    /* Appends a new node at the end.  This method is
+   defined inside LinkedList class shown above */
+    fun append(new_data: Int) {
+        /* 1. Allocate the Node &
+       2. Put in the data
+       3. Set next as null */
+        val new_node = Node(new_data)
+
+        /* 4.  */
+        if (head == null) {
+            head = Node(new_data)
+            return
+        }
+
+        /* 4. This new node is going to be the last node, so
+         make next of it as null */new_node.next = null
+
+        /* 5. Else traverse till the last node */
+        var last = head
+        while (last!!.next != null) last = last.next
+
+        /* 6. Change the next of last node */last.next = new_node
+        return
+    }
 }
