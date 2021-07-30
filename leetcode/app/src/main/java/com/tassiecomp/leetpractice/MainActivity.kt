32143f72111684ed718386eeec57061421becf43
sean3687 <yonghyun.jin11@gmail.com>
2021-07-30 10:28:51 +0900

    2021-07-30

--- leetcode/app/src/main/java/com/tassiecomp/leetpractice/MainActivity.kt
+++ leetcode/app/src/main/java/com/tassiecomp/leetpractice/MainActivity.kt
@@ -38,27 +38,84 @@ internal class LinkedList {
         /* 4. Move the head to point to new Node */
         head = new_node
     }
-    fun append(new_data: Int) {
-        /* 1. Allocate the Node &
-       2. Put in the data
-       3. Set next as null */
-        val new_node = Node(new_data)
 
-        /* 4. If the Linked List is empty, then make the
-           new node as head */if (head == null) {
-            head = Node(new_data)
-            return
+    // A complete working Java program
+    // to demonstrate deletion
+    // in singly linked list
+    internal class LinkedList {
+        var head // head of list
+                : Node? = null
+
+        /* Linked list Node*/
+        internal inner class Node(var data: Int) {
+            var next: Node? = null
         }
 
-        /* 4. This new node is going to be the last node, so
-         make next of it as null */new_node.next = null
+        /* Given a key, deletes the first
+       occurrence of key in
+     * linked list */
+        fun deleteNode(key: Int) {
+            // Store head node
+            var temp = head
+            var prev: Node? = null
 
-        /* 5. Else traverse till the last node */
-        var last = head
-        while (last!!.next != null) last = last.next
+            // If head node itself holds the key to be deleted
+            if (temp != null && temp.data == key) {
+                head = temp.next // Changed head
+                return
+            }
 
-        /* 6. Change the next of last node */last.next = new_node
-        return
+            // Search for the key to be deleted, keep track of
+            // the previous node as we need to change temp.next
+            while (temp != null && temp.data != key) {
+                prev = temp
+                temp = temp.next
+            }
+
+            // If key was not present in linked list
+            if (temp == null) return
+
+            // Unlink the node from linked list
+            prev!!.next = temp.next
+        }
+
+        /* Inserts a new Node at front of the list. */
+        fun push(new_data: Int) {
+            val new_node: Node = Node(new_data)
+            new_node.next = head
+            head = new_node
+        }
+
+        /* This function prints contents of linked list starting
+       from the given node */
+        fun printList() {
+            var tnode = head
+            while (tnode != null) {
+                print(tnode.data.toString() + " ")
+                tnode = tnode.next
+            }
+        }
+
+        companion object {
+            /* Driver program to test above functions. Ideally this
+    function should be in a separate user class. It is kept
+    here to keep code compact */
+            @JvmStatic
+            fun main(args: Array<String>) {
+                val llist = LinkedList()
+                llist.push(7)
+                llist.push(1)
+                llist.push(3)
+                llist.push(2)
+                println("\nCreated Linked list is:")
+                llist.printList()
+                llist.deleteNode(1) // Delete node with data 1
+                println(
+                    "\nLinked List after Deletion of 1:"
+                )
+                llist.printList()
+            }
+        }
     }
 }
 
